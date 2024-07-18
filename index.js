f345c6837e11352b62fbba827d0b606a7f321749
saitamachoudhary <165796559+saitamachoudhary@users.noreply.github.com>
2024-07-18 06:14:42 -0700

    18JUL !8:44

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-#!/usr/bin/env node
+// #!/usr/bin/env node
 
-const fs = require('fs-extra');
-const path = require('path');
+// const fs = require('fs-extra');
+// const path = require('path');
+// const inquirer = require('inquirer');
+// const { execSync } = require('child_process');
+
+// const TEMPLATE_PATH = path.join(__dirname, 'templates', 'default');
+
+// async function createApp() {
+//   const answers = await inquirer.prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: 'What is your project name?',
+//       default: 'my-app',
+//     },
+//   ]);
+
+//   const targetPath = path.join(process.cwd(), answers.name);
+//   await fs.copy(TEMPLATE_PATH, targetPath);
+
+//   console.log(`Success! Created ${answers.name} at ${targetPath}`);
+
+//   console.log('Installing dependencies...');
+//   execSync('npm install', { stdio: 'inherit', cwd: targetPath });
+
+//   console.log('All done! You can now start your project with:');
+//   console.log(`  cd ${answers.name}`);
+//   console.log('  npm start');
+// }
+
+// createApp().catch(err => console.error(err));
 const inquirer = require('inquirer');
-const { execSync } = require('child_process');
 
-const TEMPLATE_PATH = path.join(__dirname, 'templates', 'default');
-
-async function createApp() {
+async function testInquirer() {
   const answers = await inquirer.prompt([
     {
       type: 'input',
@@ -17,17 +43,8 @@ async function createApp() {
     },
   ]);
 
-  const targetPath = path.join(process.cwd(), answers.name);
-  await fs.copy(TEMPLATE_PATH, targetPath);
-
-  console.log(`Success! Created ${answers.name} at ${targetPath}`);
-
-  console.log('Installing dependencies...');
-  execSync('npm install', { stdio: 'inherit', cwd: targetPath });
-
-  console.log('All done! You can now start your project with:');
-  console.log(`  cd ${answers.name}`);
-  console.log('  npm start');
+  console.log(`Project name: ${answers.name}`);
 }
 
-createApp().catch(err => console.error(err));
+testInquirer().catch(err => console.error(err));
+
